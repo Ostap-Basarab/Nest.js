c2b5bd7c48b1069e02bb865fa4317aed9d892218
Os <ostap.basarab.job@gmail.com>
2024-05-08 20:00:06 +0300

    Integrate user registration service, add pcrypt, prettier

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,15 +1,15 @@
-import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 const config: PostgresConnectionOptions = {
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "mediumclone",
-    password: "123",
-    database: "mediumclone",
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: false,
-    migrations: [__dirname + '/migrations/**/*{.ts,.js}']
-}
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'mediumclone',
+  password: '123',
+  database: 'mediumclone',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+};
 
 export default config;
